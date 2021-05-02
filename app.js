43dbefa6f8743bd4d6db3d154ae655311acae8e6
Sauvic <sauvicpaulchy@gmail.com>
2021-05-02 17:50:45 +0530

    Fetching the Domain Names in the Console.

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-alert("Hello from the Vupon Extension");
+//  Get the current Domain
+var domain = window.location.hostname;
+domain = domain
+	.replace("http://", "")
+	.replace("https://", "")
+	.replace("www.", "")
+	.split(/[/?#]/)[0];
+
+chrome.runtime.sendMessage(
+	{ command: "fetch", data: { domain: domain } },
+	(response) => {
+		//  response from the database (background.html > firebase.js)
+	}
+);
